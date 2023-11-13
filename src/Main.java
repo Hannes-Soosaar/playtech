200ab626543192c4ac4a4f479670c27ca56db68c
Hannes <hannes.study@gmail.com>
2023-11-13 15:27:02 +0200

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,21 @@
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Hello world!");
+
+
+        // read files
+        // modify data
+        // write data
+
+//In the results.txt file there are 3 expected result groups separated by empty line between each group. Results.txt file should be written into the same directory as your Main class
+
+//
+//        o	Example: 163f23ed-e9a9-4e54-a5b1-4e1fc86f12f4"   "4321"           "0,80
+//        o	           player ID                          balance number, win rate are separated by space.
+//          o	win rate is big decimal number with 2 decimal characters.
+//        o	The list is ordered by player ID.   // Order by player ID
+//        o	Player is legitimate if they did not attempt any illegal action.  // need to check for illegal actions
+
+    }
+}
