53a4757e63953830f96143eaf53a415afd5a88d8
Hannes <hannes.study@gmail.com>
2023-11-14 19:21:52 +0200

    Added functionality to read in the first file

--- src/Config.java
+++ src/Config.java
@@ -1,5 +1,14 @@
 public class Config {
-        public static final String PLAYER_FILE_PATH = "./player.txt";
-        public static final String MATCH_FILE_PATH = "./match.txt";
-        public static final String REPORT_FILE_PATH = "./report.txt";
+
+    // The path for the files used, if the files need to be placed elsewhere the path should be changed to match
+    public static final String PLAYER_FILE_PATH = "./src/player_data.txt";
+    public static final String MATCH_FILE_PATH = "./src/match_data.txt";
+    public static final String REPORT_FILE_PATH = "./src/report.txt";
+    // If the order of data changes in the csv file the index can be changed to match the new configuration
+    public static final int PLAYER_ID = 0;
+    public static final int PLAYER_OPERATION = 1;
+    public static final int PLAYER_MATCH_ID = 2;
+    public static final int PLAYER_TRANSACTION_SUM = 3;
+    public static final int PLAYER_BET_PLACED = 4;
+    //
 }
--- src/Main.java
+++ src/Main.java
@@ -1,44 +1,47 @@
+
 import java.io.BufferedReader;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
 public class Main {
+     private static void readInPlayerData(){
+         List<Player> playerTransactions = new ArrayList<>();
+         try (BufferedReader readInPlayerTransactions = new BufferedReader(new FileReader(Config.PLAYER_FILE_PATH))) {
+             String transactionBuffer;
+             while ((transactionBuffer = readInPlayerTransactions.readLine()) != null) {
+                 String[] transactionDetails = transactionBuffer.split(",");
 
+                 Player transaction;
+                 if(transactionDetails.length == 5){
+                     transaction = new Player(
+                             transactionDetails[Config.PLAYER_ID],
+                             transactionDetails[Config.PLAYER_OPERATION],
+                             transactionDetails[Config.PLAYER_MATCH_ID],
+                             transactionDetails[Config.PLAYER_TRANSACTION_SUM],
+                             transactionDetails[Config.PLAYER_BET_PLACED]
+                     );
+                 }else{
+                     transaction = new Player(
+                             transactionDetails[Config.PLAYER_ID],
+                             transactionDetails[Config.PLAYER_OPERATION],
+                             transactionDetails[Config.PLAYER_MATCH_ID],
+                             transactionDetails[Config.PLAYER_TRANSACTION_SUM],
+                             ""               // an empty string will be returned if no bet was placed
+                     );
+                 }
+                 playerTransactions.add(transaction);
+             }
+         } catch (IOException e) {
+             throw new RuntimeException(e);
+         }
 
-
+    }
     public static void main(String[] args) {
-
-        System.out.println("Hello world!");
-        List<Player> playerTransactions = new ArrayList<>();
-        List<Match> playedMatches = new ArrayList<>();
-        String testString = Config.PLAYER_FILE_PATH;
-
-        try (BufferedReader readInPlayerTransactions = new BufferedReader(new FileReader(Config.PLAYER_FILE_PATH))) {
-            String transaction;  // creates a string
-            while ((transaction = readInPlayerTransactions.readLine()) != null {
-                String[] transactionDetails = transaction.split(",");
-                Player transaction = new Player(transactionDetails[0],transactionDetails[1],transactionDetails[2],
-                        transactionDetails[3],transactionDetails[4]);
-                playerTransactions.add(transaction);
-            } catch (IOException e) {
-                throw new RuntimeException(e);
-            }
+         readInPlayerData(); // todo add a return for the function
 
 
-        // read files
-        // modify data
-        // write data
-
-//In the results.txt file there are 3 expected result groups separated by empty line between each group. Results.txt file should be written into the same directory as your Main class
-//
-//        o	Example: 163f23ed-e9a9-4e54-a5b1-4e1fc86f12f4"   "4321"           "0,80
-//        o	           player ID                          balance number, win rate are separated by space.
-//          o	win rate is big decimal number with 2 decimal characters.
-//        o	The list is ordered by player ID.   // Order by player ID
-//        o	Player is legitimate if they did not attempt any illegal action.  // need to check for illegal actions
-
+    List<Match> playedMatches = new ArrayList<>();
     }
 }
