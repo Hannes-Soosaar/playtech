b4bbeaf478257fc64d459391f39b397bffa0eea6
Hannes Soosaar <135945234+Hannes-Soosaar@users.noreply.github.com>
2023-11-19 21:47:24 +0200

    Update Config.java
    corrected the file name to results.txt

--- src/Config.java
+++ src/Config.java
@@ -4,7 +4,7 @@ public class Config {
     // The path for the files used, if the files need to be placed elsewhere the path should be changed to match
     public static final String PLAYER_FILE_PATH = "./src/player_data.txt";
     public static final String MATCH_FILE_PATH = "./src/match_data.txt";
-    public static final String REPORT_FILE_PATH = "./src/report.txt";
+    public static final String REPORT_FILE_PATH = "./src/results.txt";
     // columns in the csv file for player_data.txt
     public static final int PLAYER_ID = 0;
     public static final int PLAYER_OPERATION = 1;
